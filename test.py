2973fc675e083d06013501855ce9030cbf199e1e
chris <ctaylor83@gmail.com>
2022-10-31 15:59:21 +0000

    Working on test file

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
 
-driver.get("http://www.ecosia.com")
+driver.get("http://www.ecosia.org")
+driver.implicitly_wait(2)
+driver.find_element(By.NAME, value="q").send_keys("Test Automation")
+driver.find_element(By.CLASS_NAME, value="button__text").click()
