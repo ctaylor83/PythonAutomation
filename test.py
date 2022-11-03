9b0e0ea0995a97abea68cc844a85baee1cbcab6f
chris <ctaylor83@gmail.com>
2022-11-03 11:23:26 +0000

    Fixed test script where search results were not appearing

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
 
 driver.get("http://www.ecosia.org")
 driver.implicitly_wait(2)
 driver.find_element(By.NAME, value="q").send_keys("Test Automation")
-driver.find_element(By.CLASS_NAME, value="button__text").click()
+driver.find_element(By.NAME, value="q").send_keys(Keys.ENTER)
+
